10e3b43c91431be35bacc3cabf1ec523ddd73df3
Joao Gaspar <joaopedrohenriquesgaspar@gmail.com>
2020-12-16 17:16:24 +0000

    finish v1 beta

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+(function($) {
+    'use strict';
+
+    $('.current_year').html(new Date().getFullYear())
+
+    // mobile
+    var mobile = false
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        mobile = true
+    }
+
+    if (mobile) {
+        $("h1").css("font-size", "1.2rem");
+        $(".social_mobile").show();
+    }
+
+})(jQuery);
